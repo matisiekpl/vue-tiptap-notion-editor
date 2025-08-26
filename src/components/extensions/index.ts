fc71a753ae8dc5cb692c16a70cae2daffdbc45a8
Mateusz Woźniak <matisiek11@gmail.com>
2025-08-27 01:11:01 +0200

    Enhance CustomDocument configuration and update Prosemirror.css for improved styling and layout adjustments

--- src/components/extensions/index.ts
+++ src/components/extensions/index.ts
@@ -8,6 +8,7 @@ import TextStyle from "@tiptap/extension-text-style";
 import {Color} from "@tiptap/extension-color";
 import TaskItem from "@tiptap/extension-task-item";
 import TaskList from "@tiptap/extension-task-list";
+import Document from '@tiptap/extension-document';
 import {Markdown} from "tiptap-markdown";
 import Highlight from "@tiptap/extension-highlight";
 import {InputRule} from "@tiptap/core";
@@ -24,7 +25,6 @@ import "@tiptap/extension-italic";
 import "@tiptap/extension-heading";
 import '@tiptap/extension-code'
 import {EditorContext} from "../Editor.vue";
-import Document from '@tiptap/extension-document';
 
 const CustomDocument = Document.extend({
     content: 'heading block*',
@@ -35,17 +35,17 @@ export function createDefaultExtension(context: EditorContext) {
         StarterKit.configure({
             bulletList: {
                 HTMLAttributes: {
-                    class: "list-disc list-outside leading-3 -mt-2",
+                    class: "list-disc list-outside leading-2 -mt-2",
                 },
             },
             orderedList: {
                 HTMLAttributes: {
-                    class: "list-decimal list-outside leading-3 -mt-2",
+                    class: "list-decimal list-outside leading-2 -mt-2",
                 },
             },
             listItem: {
                 HTMLAttributes: {
-                    class: "leading-normal -mb-2",
+                    class: "leading-2 -mb-2",
                 },
             },
             blockquote: {
@@ -56,13 +56,13 @@ export function createDefaultExtension(context: EditorContext) {
             codeBlock: {
                 HTMLAttributes: {
                     class:
-                        "rounded-sm bg-stone-100 p-5 font-mono font-medium text-stone-800",
+                        "rounded-lg bg-stone-100 p-5 font-mono font-medium text-stone-800",
                 },
             },
             code: {
                 HTMLAttributes: {
                     class:
-                        "rounded-md bg-stone-200 px-1.5 py-1 font-mono font-medium text-stone-900",
+                        "rounded-lg bg-stone-200 px-1.5 py-1 font-mono font-medium text-stone-900",
                     spellcheck: "false",
                 },
             },
