f874db0837fa23edc6cd69419938c9d535fa5ef1
Mateusz Woźniak <matisiek11@gmail.com>
2025-12-18 19:57:13 +0100

    fix: duplicated document node

--- src/components/extensions/index.ts
+++ src/components/extensions/index.ts
@@ -1,80 +1,89 @@
-import StarterKit from "@tiptap/starter-kit";
+import Blockquote from "@tiptap/extension-blockquote";
+import Bold from "@tiptap/extension-bold";
+import BulletList from "@tiptap/extension-bullet-list";
+import Code from "@tiptap/extension-code";
+import CodeBlock from "@tiptap/extension-code-block";
+import Document from "@tiptap/extension-document";
+import HardBreak from "@tiptap/extension-hard-break";
+import Heading from "@tiptap/extension-heading";
+import Highlight from "@tiptap/extension-highlight";
+import {Dropcursor, Gapcursor, UndoRedo} from "@tiptap/extensions";
 import HorizontalRule from "@tiptap/extension-horizontal-rule";
-import TiptapLink from "@tiptap/extension-link";
-import TiptapImage from "@tiptap/extension-image";
-import Placeholder from "@tiptap/extension-placeholder";
-import TiptapUnderline from "@tiptap/extension-underline";
-import TextStyle from "@tiptap/extension-text-style";
+import Italic from "@tiptap/extension-italic";
 import {Color} from "@tiptap/extension-color";
+import ListItem from "@tiptap/extension-list-item";
+import {Markdown} from "tiptap-markdown";
+import OrderedList from "@tiptap/extension-ordered-list";
+import Paragraph from "@tiptap/extension-paragraph";
+import TiptapImage from "@tiptap/extension-image";
+import {ListKeymap} from "@tiptap/extension-list";
+import Placeholder from "@tiptap/extension-placeholder";
+import Strike from "@tiptap/extension-strike";
 import TaskItem from "@tiptap/extension-task-item";
 import TaskList from "@tiptap/extension-task-list";
-import Document from '@tiptap/extension-document';
-import {Markdown} from "tiptap-markdown";
-import Highlight from "@tiptap/extension-highlight";
+import Text from "@tiptap/extension-text";
+import TextStyle from "@tiptap/extension-text-style";
+import TiptapLink from "@tiptap/extension-link";
+import TiptapUnderline from "@tiptap/extension-underline";
 import {InputRule} from "@tiptap/core";
 import {createSlashCommand} from "./slashExtension";
 import UploadImagesPlugin from "../plugins/uploadImages.ts";
 import UpdatedImage from "./updatedImage.ts";
 import Attachment from "./attachment";
 import {createPasteUploads} from "./pasteUploads";
-import "@tiptap/extension-list";
-import "@tiptap/extension-blockquote";
-import "@tiptap/extension-code-block";
-import "@tiptap/extension-strike";
-import "@tiptap/extension-italic";
-import "@tiptap/extension-heading";
-import '@tiptap/extension-code';
-import '@tiptap/extension-bold';
 import {EditorContext} from "../Editor.vue";
 
 const CustomDocument = Document.extend({
-    content: 'heading block*',
-})
+    content: "heading block*",
+});
 
 export function createDefaultExtension(context: EditorContext) {
-    const extensions = [
-        StarterKit.configure({
-            bulletList: {
-                HTMLAttributes: {
-                    class: "list-disc list-outside leading-2 -mt-2",
-                },
-            },
-            orderedList: {
-                HTMLAttributes: {
-                    class: "list-decimal list-outside leading-2 -mt-2",
-                },
-            },
-            listItem: {
-                HTMLAttributes: {
-                    class: "leading-2 -mb-2",
-                },
-            },
-            blockquote: {
-                HTMLAttributes: {
-                    class: "border-l-4 border-stone-700",
-                },
-            },
-            codeBlock: {
-                HTMLAttributes: {
-                    class:
-                        "rounded-lg bg-stone-100 p-5 font-mono font-medium text-stone-800",
-                },
-            },
-            code: {
-                HTMLAttributes: {
-                    class:
-                        "rounded-lg bg-stone-200 px-1.5 py-1 font-mono font-medium text-stone-900",
-                    spellcheck: "false",
-                },
-            },
-            horizontalRule: false,
-            dropcursor: {
-                color: "#DBEAFE",
-                width: 4,
-            },
-            gapcursor: false,
-        }),
+    const documentExtension = context.structuredDocument ? CustomDocument : Document;
 
+    return [
+        documentExtension,
+        Paragraph,
+        Text,
+        Heading,
+        Blockquote.configure({
+            HTMLAttributes: {
+                class: "border-l-4 border-stone-700",
+            },
+        }),
+        BulletList.configure({
+            HTMLAttributes: {
+                class: "list-disc list-outside leading-2 -mt-2",
+            },
+        }),
+        OrderedList.configure({
+            HTMLAttributes: {
+                class: "list-decimal list-outside leading-2 -mt-2",
+            },
+        }),
+        ListItem.configure({
+            HTMLAttributes: {
+                class: "leading-2 -mb-2",
+            },
+        }),
+        CodeBlock.configure({
+            HTMLAttributes: {
+                class:
+                    "rounded-lg bg-stone-100 p-5 font-mono font-medium text-stone-800",
+            },
+        }),
+        Code.configure({
+            HTMLAttributes: {
+                class:
+                    "rounded-lg bg-stone-200 px-1.5 py-1 font-mono font-medium text-stone-900",
+                spellcheck: "false",
+            },
+        }),
+        HardBreak,
+        UndoRedo,
+        Dropcursor.configure({
+            color: "#DBEAFE",
+            width: 4,
+        }),
         HorizontalRule.extend({
             addInputRules() {
                 return [
@@ -100,12 +109,17 @@ export function createDefaultExtension(context: EditorContext) {
                 class: "!mt-4 !mb-4 border-t border-stone-300",
             },
         }),
+        Bold,
+        Italic,
+        Strike,
+        ListKeymap,
         TiptapLink.configure({
             HTMLAttributes: {
                 class:
                     "text-stone-400 underline underline-offset-[3px] hover:text-stone-600 transition-colors cursor-pointer",
             },
         }),
+        Gapcursor,
         TiptapImage.extend({
             addProseMirrorPlugins() {
                 return [UploadImagesPlugin()];
@@ -157,7 +171,5 @@ export function createDefaultExtension(context: EditorContext) {
             transformCopiedText: true,
         }),
         createSlashCommand(context),
-    ]
-    if (context.structuredDocument) extensions.push(CustomDocument);
-    return extensions;
+    ];
 }
