e0f37e21e1dd2b4749c6b62ab729cf27e55477a5
Mateusz Woźniak <matisiek11@gmail.com>
2026-02-09 20:29:19 +0100

    feat: adjust colors

--- src/components/extensions/YoutubePlaceholder.vue
+++ src/components/extensions/YoutubePlaceholder.vue
@@ -14,7 +14,7 @@
         v-model="url"
         type="text"
         placeholder="Paste YouTube link here..."
-        class="file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-white px-3 py-1 text-base outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm focus-visible:border-ring focus-visible:ring-ring/100 focus-visible:ring-[2px] shadow/20 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive"
+        class="file:text-foreground placeholder:text-muted-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-white px-3 py-1 text-base outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm focus-visible:border-ring focus-visible:ring-ring/100 focus-visible:ring-[2px] shadow/20 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive"
         @keydown.enter="submit"
         @mousedown.stop
       />
